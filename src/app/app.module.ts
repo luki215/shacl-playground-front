4698538e835636491f863d27adeb4ce6db8b969a
Lukas Brezina <lukas.brezina@coex.cz>
2019-12-10 16:40:48 +0100

    feat(editor): remove unused component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { EditorComponent } from './editor/editor.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CodemirrorModule } from '@ctrl/ngx-codemirror';
 
@@ -21,8 +20,7 @@ import {MatButtonToggleModule} from '@angular/material/button-toggle';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    EditorComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
